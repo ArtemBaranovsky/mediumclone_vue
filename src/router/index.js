8ed6b120bd142f2fe67431f594221f537714b456
Artem Baranovsky <artem.baranovsky1980@gmail.com>
2021-09-01 18:52:32 +0300

    Added user register page, added external styles.

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 // import Vue from 'vue'   // not used in Vue 3 init
 // import VueRouter from 'vue-router'  // not used in Vue 3 init
 import {createRouter, createWebHistory} from 'vue-router'
-import Home from '@/views/Home.vue'
+import Home from '@/views/Home'
+import Register from "@/views/Register";
 
 // Vue.use(VueRouter)   // not used in Vue 3 init
 
@@ -12,13 +13,10 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
 ]
 
 // Vue 3 init
